0b2dab302f5e3872e51d85d1c3272b46b713a37c
shivaGprasadh <49456600+shivaGprasadh@users.noreply.github.com>
2025-05-28 09:11:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,4 +56,4 @@ def create_app():
 if __name__ == '__main__':
     # Initialize the app by calling create_app() which registers routes
     app = create_app()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5005, debug=True)
